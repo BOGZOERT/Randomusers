bd4261a3ceb4e688271ac1ca5d58c416f84947e2
Alexander <46847979+alyxe-code@users.noreply.github.com>
2021-09-13 22:40:33 +0200

    UserDetails reform (#1)
    Co-authored-by: BOGZOERT <54588921+BOGZOERT@users.noreply.github.com>

--- lib/user/details/UserDetails.dart
+++ lib/user/details/UserDetails.dart
@@ -1,8 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/widgets.dart';
+import 'package:randomusers/storage/entity/UserSM.dart';
+import 'package:randomusers/user/list/UserListBloc.dart';
+import 'package:randomusers/user/list/UserListState.dart';
+import 'package:randomusers/user/list/UserListEvent.dart';
+
+
 
 class UsersDetails extends StatelessWidget {
+
+
+  Widget _spacing(BuildContext context) {
+    final responsive = MediaQuery
+        .of(context)
+        .size
+        .height;
+    return new Row(
+      children: <Widget>[
+        new Expanded(
+          child: new SizedBox(
+            height: responsive * 0.01,
+            width: 500.0,
+          ),
+        )
+      ],
+    );
+  }
+  final UserSM userDetails;
+  UsersDetails({required this.userDetails}) : super();
+
   @override
   Widget build(BuildContext context) {
-    return Text("User Details");
+    // final double responsive = MediaQuery
+    //     .of(context)
+    //     .size
+    //     .height;
+
+
+
+    return BlocBuilder<UserListBloc, UserListState>(
+      builder: (context, state) =>
+          FutureBuilder(
+            future: onLoaded(userListBloc(context)),
+            builder: (context, _) =>
+             InkWell(
+
+                  child: Column(
+
+                        children: [
+                        CircleAvatar(
+                          radius: 60,
+                          foregroundImage: NetworkImage(userDetails.pictureLarge ?? "Error loading image" ),
+                          backgroundColor: Color(0xff363030),
+                        ),
+                    Text(
+                      "${userDetails.firstName} ${userDetails.lastName}",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(userDetails.email ?? "No email"),
+                    ],
+                ),
+             ),
+          ),
+    );
+  }
+
+  UserListBloc userListBloc(BuildContext context) => context.read();
+
+  Future<void> onLoaded(UserListBloc bloc) async {
+    bloc.add(UserListEvent.initialized);
   }
 }
--- lib/user/list/UserListItem.dart
+++ lib/user/list/UserListItem.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:randomusers/storage/entity/UserSM.dart';
+import 'package:randomusers/user/details/UserDetails.dart';
 
 class UserListItem extends StatelessWidget {
   final UserSM user;
-
   UserListItem({required this.user}) : super();
 
+
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(20),
+  Widget build(BuildContext context,) {
+
+    return InkWell(
+
       child: Column(
+
         children: [
+          CircleAvatar(
+            radius: 60,
+            foregroundImage: NetworkImage(user.pictureLarge ?? "Error loading image" ),
+            backgroundColor: Color(0xff363030),
+          ),
           Text(
             "${user.firstName} ${user.lastName}",
             style: TextStyle(
@@ -19,9 +28,20 @@ class UserListItem extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
           ),
-          Text(user.email ?? "No email")
+          Text(user.email ?? "No email"),
         ],
+
       ),
+      onTap: (){
+        Navigator.push(
+          context,
+          new MaterialPageRoute(
+             builder: (context) => UsersDetails(),
+          ),
+        );
+
+
+      },
     );
   }
 }
